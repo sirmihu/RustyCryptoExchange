9582e5a7e20a3468c6d64f07dd2d5ca74de548c7
michalp <michalp@SKY-MICHALP-01>
2024-01-29 16:19:17 +0100

    connection to external API added

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
-fn main() {
-    let x = 42;
-    let y = 12;
-    println!("Hello, world!");
-    println!("x + y = {}", x + y);
+use reqwest;
+use serde_json;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let api_key = "*";
+    let base_url = "https://rest.coinapi.io";
+    let asset_id_base = "BTC";
+
+    let url = format!("{}/v1/exchangerate/{}", base_url, asset_id_base);
+
+    let client = reqwest::Client::new();
+
+    let response = client.get(&url)
+        .header("X-CoinAPI-Key", api_key)
+        .send()
+        .await?;
+
+    if response.status().is_success() {
+        let body = response.bytes().await?;
+        let body_str = String::from_utf8_lossy(&body);
+
+        let result: serde_json::Value = serde_json::from_str(&body_str)?;
+
+        println!("{:?}", result);
+    } else {
+        println!("Błąd zapytania: {:?}", response.status());
+    }
+
+    Ok(())
 }
